3a8b0993c54013f0443e75873019cc43f6b2dc72
Denis <Oleborn@mail.ru>
2024-12-13 07:49:08 +0700

    Update controller

--- src/main/java/oleborn/testcicd/HelloController.java
+++ src/main/java/oleborn/testcicd/HelloController.java
@@ -10,4 +10,9 @@ public class HelloController {
     public String home() {
         return "Hello from Spring Boot in Docker!";
     }
+
+    @GetMapping("/testCICD")
+    public String testCICD() {
+        return "Ебать-колотить, работает!";
+    }
 }
